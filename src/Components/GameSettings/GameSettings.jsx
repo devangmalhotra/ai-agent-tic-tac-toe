e40e1d232c64bfede8620d85c14d6779c4f0da7a
devangmalhotra <malh2226@mylaurier.ca>
2025-09-06 19:31:15 -0400

    updated game form

--- src/Components/GameSettings/GameSettings.jsx
+++ src/Components/GameSettings/GameSettings.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import './GameSettings.css'
 
 function GameSettings() {
   return (
-    <div>GameSettings</div>
+    <div id='form-container'>
+        <form id='game-form' action="">
+            <label htmlFor="algo-type">Select Algorithm Type:</label>
+            <input type="text" />
+        </form>
+    </div>
   )
 }
 
