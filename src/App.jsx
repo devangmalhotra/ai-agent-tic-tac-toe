9f20a995f8784300df247f45dea0a29df306eee9
devangmalhotra <malh2226@mylaurier.ca>
2025-09-06 19:24:56 -0400

    added GameSettings component

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Title from './Components/Title/Title'
+import GameSettings from './Components/GameSettings/GameSettings'
 
 function App() {
 
   return (
-    <Title />
+    <div>
+      <Title />
+      <GameSettings />
+    </div>
+    
   )
 }
 
