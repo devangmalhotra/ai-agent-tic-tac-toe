000b99e7a3656caf5a408c9655d3547220d12d80
devangmalhotra <malh2226@mylaurier.ca>
2025-09-08 13:48:38 -0400

    updated files

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Title from './Components/Title/Title'
-import GameSettings from './Components/GameSettings/GameSettings'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import GamePage from './Pages/GamePage/GamePage'
 
--- src/Pages/GamePage/GamePage.jsx
+++ src/Pages/GamePage/GamePage.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Title from '../../Components/Title/Title'
+import GameSettings from '../../Components/GameSettings/GameSettings'
 
 function GamePage() {
   return (
-    <div>GamePage</div>
+    <div>
+      <Title />
+      <GameSettings />
+    </div>
   )
 }
 
