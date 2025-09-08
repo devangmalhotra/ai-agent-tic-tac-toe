e470ba20faec33b7b64824191bec1d4a369d9238
devangmalhotra <malh2226@mylaurier.ca>
2025-09-08 15:43:44 -0400

    updated files

--- src/Components/GameSettings/GameSettings.jsx
+++ src/Components/GameSettings/GameSettings.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import './GameSettings.css'
 
 function GameSettings() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("test");
+  }
+
   return (
     <div id='form-container'>
-        <form id='game-form' action="">
+        <form id='game-form' action="" onSubmit={handleSubmit}>
             <label htmlFor="algo-type">Select Algorithm Type:</label>
             <select name="algo-type" id="">
                 <option value="minimax">MiniMax</option>
