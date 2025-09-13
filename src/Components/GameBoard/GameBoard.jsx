ce44ae4b152ca2e26987067c0d6e4b83510c4734
devangmalhotra <malh2226@mylaurier.ca>
2025-09-12 23:22:57 -0400

    added cells

--- src/Components/GameBoard/GameBoard.jsx
+++ src/Components/GameBoard/GameBoard.jsx
@@ -3,7 +3,17 @@ import './GameBoard.css'
 
 function GameBoard() {
   return (
-    <div id='gameboard-container'></div>
+    <div id='gameboard-container'>
+        <div id='1-1' className='gameboard-cell'></div>
+        <div id='1-2' className='gameboard-cell'></div>
+        <div id='1-3' className='gameboard-cell'></div>
+        <div id='2-1' className='gameboard-cell'></div>
+        <div id='2-2' className='gameboard-cell'></div>
+        <div id='2-3' className='gameboard-cell'></div>
+        <div id='3-1' className='gameboard-cell'></div>
+        <div id='3-2' className='gameboard-cell'></div>
+        <div id='3-3' className='gameboard-cell'></div>
+    </div>
   )
 }
 
