387a58c120e231774acecd880207a2511badf942
devangmalhotra <malh2226@mylaurier.ca>
2025-09-14 00:33:20 -0400

    updated files

--- src/Components/GameBoard/GameBoard.jsx
+++ src/Components/GameBoard/GameBoard.jsx
@@ -4,8 +4,12 @@ import './GameBoard.css'
 function GameBoard() {
   return (
     <div id='gameboard-container'>
-        <div id='1-1' className='gameboard-cell'></div>
-        <div id='1-2' className='gameboard-cell'></div>
+        <div id='1-1' className='gameboard-cell'>
+            <div className='x-piece'></div>
+        </div>
+        <div id='1-2' className='gameboard-cell'>
+            <div className='o-piece'></div>
+        </div>
         <div id='1-3' className='gameboard-cell'></div>
         <div id='2-1' className='gameboard-cell'></div>
         <div id='2-2' className='gameboard-cell'></div>
