1c52ae2cf189a83b1ce1bcf5cb7cf3ab052ecdc3
devangmalhotra <malh2226@mylaurier.ca>
2025-09-14 23:28:07 -0400

    updated files

--- src/Components/GameBoard/GameBoard.jsx
+++ src/Components/GameBoard/GameBoard.jsx
@@ -8,24 +8,28 @@ function GameBoard() {
    const  [turn, setTurn] = useState(1); //1 for player, 2 for AI
    
    const handleTurnChange = (currTurn) => {
-        
-   }
+        if(turn === 1) {
+            setTurn(2);
+        } else {
+            setTurn(1);
+        }
+   };
 
   return (
     <div id='gameboard-container'>
-        <div id='1-1' className='gameboard-cell'>
+        <div id='1-1' className='gameboard-cell' onClick={handleTurnChange}>
             <XPiece />
         </div>
-        <div id='1-2' className='gameboard-cell'>
+        <div id='1-2' className='gameboard-cell' onClick={handleTurnChange}>
             <OPiece />
         </div>
-        <div id='1-3' className='gameboard-cell'></div>
-        <div id='2-1' className='gameboard-cell'></div>
-        <div id='2-2' className='gameboard-cell'></div>
-        <div id='2-3' className='gameboard-cell'></div>
-        <div id='3-1' className='gameboard-cell'></div>
-        <div id='3-2' className='gameboard-cell'></div>
-        <div id='3-3' className='gameboard-cell'></div>
+        <div id='1-3' className='gameboard-cell' onClick={handleTurnChange}></div>
+        <div id='2-1' className='gameboard-cell' onClick={handleTurnChange}></div>
+        <div id='2-2' className='gameboard-cell' onClick={handleTurnChange}></div>
+        <div id='2-3' className='gameboard-cell' onClick={handleTurnChange}></div>
+        <div id='3-1' className='gameboard-cell' onClick={handleTurnChange}></div>
+        <div id='3-2' className='gameboard-cell' onClick={handleTurnChange}></div>
+        <div id='3-3' className='gameboard-cell' onClick={handleTurnChange}></div>
     </div>
   )
 }
