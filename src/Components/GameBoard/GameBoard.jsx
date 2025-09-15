138a373a224c701bef44cc9396848c3bdfd95894
devangmalhotra <malh2226@mylaurier.ca>
2025-09-14 23:02:59 -0400

    updated files

--- src/Components/GameBoard/GameBoard.jsx
+++ src/Components/GameBoard/GameBoard.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 import './GameBoard.css'
 import XPiece from '../XPiece/XPiece'
 import OPiece from '../OPiece/OPiece'
+import { useState } from 'react'
 
 function GameBoard() {
+   const  [turn, setTurn] = useState(1); //1 for player, 2 for AI
+   
+   const handleTurnChange = (currTurn) => {
+    
+   }
+
   return (
     <div id='gameboard-container'>
         <div id='1-1' className='gameboard-cell'>
