5cce06458de2bc12f0e834c9aeb4d68d422db6ce
Devang Malhotra <119973585+devangmalhotra@users.noreply.github.com>
2025-10-30 16:42:03 -0400

    updated files

--- src/Pages/GamePage/GamePage.jsx
+++ src/Pages/GamePage/GamePage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Title from '../../Components/Title/Title'
 import GameBoard from '../../Components/GameBoard/GameBoard'
+import userStats from '../../Components/userStats/userStats'
 
 function GamePage() {
   return (
     <div>
       <Title />
+      <userStats />
       <GameBoard />
     </div>
   )
