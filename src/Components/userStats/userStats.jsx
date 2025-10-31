e4a830ed4d87b070ec854665cd4900d80ddee2ad
Devang Malhotra <119973585+devangmalhotra@users.noreply.github.com>
2025-10-30 22:55:25 -0400

    updated files

--- src/Components/userStats/userStats.jsx
+++ src/Components/userStats/userStats.jsx
@@ -6,16 +6,17 @@ function UserStats() {
     const [statsMenuOpened, setStatsMenuOpened] = useState(false);
 
     const handleStatsButtonClick = () => {
-        if (statsMenuOpened) {
+        if (!statsMenuOpened) {
             setStatsMenuOpened(true);
         } else {
             setStatsMenuOpened(false);
         }
+        console.log(statsMenuOpened)
     }
 
   return (
     <div id='stats-container'>
-        <button id='stats-button' onClick={handleStatsButtonClick}>Game Stats <span id='arrow-collapse-icon'>▶</span></button>
+        <button id='stats-button' onClick={handleStatsButtonClick}>Game Stats <span className={statsMenuOpened ? 'stats-menu-opened' : ''}id='arrow-collapse-icon'>▶</span></button>
     </div>
   )
 }
