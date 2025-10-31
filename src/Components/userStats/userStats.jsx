f02148c964c080e470df843fa520764bfce52878
Devang Malhotra <119973585+devangmalhotra@users.noreply.github.com>
2025-10-31 17:02:33 -0400

    updated files

--- src/Components/userStats/userStats.jsx
+++ src/Components/userStats/userStats.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import './UserStats.css'
 import { useState } from 'react'
 
-function UserStats() {
+function UserStats(props) {
     const [statsMenuOpened, setStatsMenuOpened] = useState(false);
+    const [statsObj, setStatsObj] = useState({ playerCurrStreak: 0, playerHighestStreak: 0, globalStreak: 0 });
 
     const handleStatsButtonClick = () => {
         if (!statsMenuOpened) {
@@ -13,13 +14,24 @@ function UserStats() {
         }
     }
 
+    const getUserId = () => {
+        const userId = localStorage.getItem('userId');
+
+        if (!userId) {
+            const uuid = crypto.randomUUID();
+            localStorage.setItem('userId', uuid);
+        }
+
+        return userId;
+    }
+
   return (
     <div id='stats-container'>
         <button id='stats-button' onClick={handleStatsButtonClick}>Game Stats <span className={statsMenuOpened ? 'stats-menu-opened' : ''}id='arrow-collapse-icon'>▶</span></button>
         <div id='stats-box' className={statsMenuOpened ? '' : 'stats-menu-hidden'}>
-            <h4>Your current streak: </h4>
-            <h4>Your highest streak: </h4>
-            <h4>Highest streak of all time: </h4>
+            <h4>Your current streak: <span className='statNum'>{statsObj.playerCurrStreak}</span></h4>
+            <h4>Your highest streak: <span className='statNum'>{statsObj.playerHighestStreak}</span></h4>
+            <h4>Highest streak of all time: <span className='statNum'>{statsObj.globalStreak}</span></h4>
         </div>
     </div>
   )
