6bb490672f7bcf653839a03d068afc08a507b229
Devang Malhotra <119973585+devangmalhotra@users.noreply.github.com>
2025-10-30 22:28:19 -0400

    updated files

--- src/Components/userStats/userStats.jsx
+++ src/Components/userStats/userStats.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import './UserStats.css'
+import { useState } from 'react'
 
 function UserStats() {
+    const [statsMenuOpened, setStatsMenuOpened] = useState(false);
+
+    const handleStatsButtonClick = () => {
+        if (statsMenuOpened) {
+            setStatsMenuOpened(true);
+        } else {
+            setStatsMenuOpened(false);
+        }
+    }
+
   return (
     <div id='stats-container'>
-        <button id='stats-button'>Game Stats <span id='arrow-collapse-icon'>▶</span></button>
+        <button id='stats-button' onClick={handleStatsButtonClick}>Game Stats <span id='arrow-collapse-icon'>▶</span></button>
     </div>
   )
 }
