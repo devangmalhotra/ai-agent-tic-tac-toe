f334e12fc22b85f508a9c68bc785f26f63612a8f
Devang Malhotra <119973585+devangmalhotra@users.noreply.github.com>
2025-11-02 19:55:49 -0500

    updated files

--- src/Pages/MainSelection/MainSelection.jsx
+++ src/Pages/MainSelection/MainSelection.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Title from '../../Components/Title/Title'
 import GameSettings from '../../Components/GameSettings/GameSettings'
 import './MainSelection.css'
-import UserStats from '../../Components/TempFolder/userStats'
+import UserStats from '../../Components/UserStats/userStats'
 
 function MainSelection() {
   return (
